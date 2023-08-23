a120e8a523a90034d677f53075c550964e877adc
HyunseokLEE <unducklee@gmail.com>
2023-08-23 18:51:56 +0900

    feat: dark mode

--- 3주차과제/clock/clock.js
+++ 3주차과제/clock/clock.js
@@ -59,3 +59,14 @@ document.getElementById("addAlarmButton").addEventListener("click", () => {
 });
 
 updateBatteryLevel();
+
+const darkModeSwitch = document.getElementById("darkModeSwitch");
+const body = document.body;
+
+darkModeSwitch.addEventListener("change", () => {
+  if (darkModeSwitch.checked) {
+    body.classList.add("dark-mode");
+  } else {
+    body.classList.remove("dark-mode");
+  }
+});
